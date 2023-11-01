28440d9ba810863c27c684bb2ca57a0fbc72f838
Allan Juma <allancjuma@gmail.com>
2023-11-01 03:46:21 +0300

    port update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { errorHandler } = require("@middleware");
 const { DBConn, allowedOrigins } = require("@config");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(helmet());
 
