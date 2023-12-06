e7038271c725f571a603648b9e171572692ea75b
Allan Juma <106666946+allanXQ@users.noreply.github.com>
2023-12-06 23:20:51 +0300

    self ping

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const { errorHandler } = require("@middleware");
 const { DBConn, allowedOrigins } = require("@config");
+const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT || 5005;
@@ -24,5 +25,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/tinypesa/", require("@routes"));
 
 app.use(errorHandler);
+
+const pingInterval = 840000; // 14 minutes in milliseconds
+
+function pingSelf() {
+  axios
+    .get("https://tinywebhook.onrender.com/api/v1/user/user-info")
+    .then((response) => {
+      console.log("Service pinged successfully:", response.status);
+    })
+    .catch((error) => {
+      console.error("Error pinging service:", error);
+    });
+}
+
+setInterval(pingSelf, pingInterval);
 
 DBConn(app, port);
